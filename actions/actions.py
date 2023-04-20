231d571ff8813fe4eb98f6b7a0c8cdbc140abcd6
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-04-20 21:41:13 +0200

    Comment unused actions

--- actions/actions.py
+++ actions/actions.py
@@ -117,104 +117,104 @@ class ActionLoadSessionFirst(Action):
         return [SlotSet("session_loaded", session_loaded)]
 
 
-class ActionLoadSessionNotFirst(Action):
+# class ActionLoadSessionNotFirst(Action):
 
-    def name(self) -> Text:
-        return "action_load_session_not_first"
+#     def name(self) -> Text:
+#         return "action_load_session_not_first"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        prolific_id = tracker.current_state()['sender_id']
-        session_num = tracker.get_slot("session_num")
+#         prolific_id = tracker.current_state()['sender_id']
+#         session_num = tracker.get_slot("session_num")
         
-        session_loaded = True
-        mood_prev = ""
+#         session_loaded = True
+#         mood_prev = ""
         
-        conn = mysql.connector.connect(
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD,
-            host=DATABASE_HOST,
-            port=DATABASE_PORT,
-            database='db'
-        )
-        cur = conn.cursor(prepared=True)
+#         conn = mysql.connector.connect(
+#             user=DATABASE_USER,
+#             password=DATABASE_PASSWORD,
+#             host=DATABASE_HOST,
+#             port=DATABASE_PORT,
+#             database='db'
+#         )
+#         cur = conn.cursor(prepared=True)
         
-        # get user name from database
-        query = ("SELECT name FROM users WHERE prolific_id = %s")
-        cur.execute(query, [prolific_id])
-        user_name_result = cur.fetchone()
+#         # get user name from database
+#         query = ("SELECT name FROM users WHERE prolific_id = %s")
+#         cur.execute(query, [prolific_id])
+#         user_name_result = cur.fetchone()
         
-        if user_name_result is None:
-            session_loaded = False
+#         if user_name_result is None:
+#             session_loaded = False
             
-        else:
-            user_name_result = user_name_result[0]
+#         else:
+#             user_name_result = user_name_result[0]
             
-            # check if user has done previous session before '
-            # (i.e., if session data is saved from previous session)
-            query = ("SELECT * FROM sessiondata WHERE prolific_id = %s and session_num = %s and response_type = %s")
-            cur.execute(query, [prolific_id, str(int(session_num) - 1), "state_1"])
-            done_previous_result = cur.fetchone()
+#             # check if user has done previous session before '
+#             # (i.e., if session data is saved from previous session)
+#             query = ("SELECT * FROM sessiondata WHERE prolific_id = %s and session_num = %s and response_type = %s")
+#             cur.execute(query, [prolific_id, str(int(session_num) - 1), "state_1"])
+#             done_previous_result = cur.fetchone()
             
-            if done_previous_result is None:
-                session_loaded = False
+#             if done_previous_result is None:
+#                 session_loaded = False
                 
-            else:
-                # check if user has not done this session before
-                # checks if some data on this session is already saved in database
-                # this basically means that it checks whether the user has already 
-                # completed the session part until the dropout question before,
-                # since that is when we first save something to the database
-                session_loaded = check_session_not_done_before(cur, prolific_id, 
-                                                               session_num)
+#             else:
+#                 # check if user has not done this session before
+#                 # checks if some data on this session is already saved in database
+#                 # this basically means that it checks whether the user has already 
+#                 # completed the session part until the dropout question before,
+#                 # since that is when we first save something to the database
+#                 session_loaded = check_session_not_done_before(cur, prolific_id, 
+#                                                                session_num)
                 
-                if session_loaded:
-                    # Get mood from previous session
-                    query = ("SELECT response_value FROM sessiondata WHERE prolific_id = %s and session_num = %s and response_type = %s")
-                    cur.execute(query, [prolific_id, str(int(session_num) - 1), "mood"])
-                    mood_prev = cur.fetchone()[0]
+#                 if session_loaded:
+#                     # Get mood from previous session
+#                     query = ("SELECT response_value FROM sessiondata WHERE prolific_id = %s and session_num = %s and response_type = %s")
+#                     cur.execute(query, [prolific_id, str(int(session_num) - 1), "mood"])
+#                     mood_prev = cur.fetchone()[0]
                     
         
-        conn.close()
+#         conn.close()
 
         
-        return [SlotSet("user_name_slot_not_first", user_name_result),
-                SlotSet("mood_prev_session", mood_prev),
-                SlotSet("session_loaded", session_loaded)]
+#         return [SlotSet("user_name_slot_not_first", user_name_result),
+#                 SlotSet("mood_prev_session", mood_prev),
+#                 SlotSet("session_loaded", session_loaded)]
         
         
     
-class ActionSaveNameToDB(Action):
+# class ActionSaveNameToDB(Action):
 
-    def name(self) -> Text:
-        return "action_save_name_to_db"
+#     def name(self) -> Text:
+#         return "action_save_name_to_db"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         
-        now = datetime.now()
-        formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
+#         now = datetime.now()
+#         formatted_date = now.strftime('%Y-%m-%d %H:%M:%S')
 
-        conn = mysql.connector.connect(
-            user=DATABASE_USER,
-            password=DATABASE_PASSWORD,
-            host=DATABASE_HOST,
-            port=DATABASE_PORT,
-            database='db'
-        )
-        cur = conn.cursor(prepared=True)
-        query = "INSERT INTO users(prolific_id, name, time) VALUES(%s, %s, %s)"
-        queryMatch = [tracker.current_state()['sender_id'], 
-                      tracker.get_slot("user_name_slot"),
-                      formatted_date]
-        cur.execute(query, queryMatch)
-        conn.commit()
-        conn.close()
+#         conn = mysql.connector.connect(
+#             user=DATABASE_USER,
+#             password=DATABASE_PASSWORD,
+#             host=DATABASE_HOST,
+#             port=DATABASE_PORT,
+#             database='db'
+#         )
+#         cur = conn.cursor(prepared=True)
+#         query = "INSERT INTO users(prolific_id, name, time) VALUES(%s, %s, %s)"
+#         queryMatch = [tracker.current_state()['sender_id'], 
+#                       tracker.get_slot("user_name_slot"),
+#                       formatted_date]
+#         cur.execute(query, queryMatch)
+#         conn.commit()
+#         conn.close()
 
-        return []
+#         return []
     
 
 class ActionSaveActivityExperience(Action):
@@ -296,45 +296,45 @@ class ActionSaveSession(Action):
     
     
 
-class ValidateActivityExperienceForm(FormValidationAction):
-    def name(self) -> Text:
-        return 'validate_activity_experience_form'
+# class ValidateActivityExperienceForm(FormValidationAction):
+#     def name(self) -> Text:
+#         return 'validate_activity_experience_form'
 
-    def validate_activity_experience_slot(
-            self, value: Text, dispatcher: CollectingDispatcher,
-            tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
-        # pylint: disable=unused-argument
-        """Validate activity_experience_slot input."""
-        last_utterance = get_latest_bot_utterance(tracker.events)
+#     def validate_activity_experience_slot(
+#             self, value: Text, dispatcher: CollectingDispatcher,
+#             tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
+#         # pylint: disable=unused-argument
+#         """Validate activity_experience_slot input."""
+#         last_utterance = get_latest_bot_utterance(tracker.events)
 
-        if last_utterance != 'utter_ask_activity_experience_slot':
-            return {"activity_experience_slot": None}
+#         if last_utterance != 'utter_ask_activity_experience_slot':
+#             return {"activity_experience_slot": None}
 
-        # people should either type "none" or say a bit more
-        if not (len(value) >= 10 or "none" in value.lower()):
-            dispatcher.utter_message(response="utter_provide_more_detail")
-            return {"activity_experience_slot": None}
+#         # people should either type "none" or say a bit more
+#         if not (len(value) >= 10 or "none" in value.lower()):
+#             dispatcher.utter_message(response="utter_provide_more_detail")
+#             return {"activity_experience_slot": None}
 
-        return {"activity_experience_slot": value}
+#         return {"activity_experience_slot": value}
     
 
-class ValidateActivityExperienceModForm(FormValidationAction):
-    def name(self) -> Text:
-        return 'validate_activity_experience_mod_form'
+# class ValidateActivityExperienceModForm(FormValidationAction):
+#     def name(self) -> Text:
+#         return 'validate_activity_experience_mod_form'
 
-    def validate_activity_experience_mod_slot(
-            self, value: Text, dispatcher: CollectingDispatcher,
-            tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
-        # pylint: disable=unused-argument
-        """Validate activity_experience_mod_slot input."""
-        last_utterance = get_latest_bot_utterance(tracker.events)
+#     def validate_activity_experience_mod_slot(
+#             self, value: Text, dispatcher: CollectingDispatcher,
+#             tracker: Tracker, domain: Dict[Text, Any]) -> Dict[Text, Any]:
+#         # pylint: disable=unused-argument
+#         """Validate activity_experience_mod_slot input."""
+#         last_utterance = get_latest_bot_utterance(tracker.events)
 
-        if last_utterance != 'utter_ask_activity_experience_mod_slot':
-            return {"activity_experience_mod_slot": None}
+#         if last_utterance != 'utter_ask_activity_experience_mod_slot':
+#             return {"activity_experience_mod_slot": None}
 
-        # people should either type "none" or say a bit more
-        if not (len(value) >= 5 or "none" in value.lower()):
-            dispatcher.utter_message(response="utter_provide_more_detail")
-            return {"activity_experience_mod_slot": None}
+#         # people should either type "none" or say a bit more
+#         if not (len(value) >= 5 or "none" in value.lower()):
+#             dispatcher.utter_message(response="utter_provide_more_detail")
+#             return {"activity_experience_mod_slot": None}
 
-        return {"activity_experience_mod_slot": value}
+#         return {"activity_experience_mod_slot": value}
