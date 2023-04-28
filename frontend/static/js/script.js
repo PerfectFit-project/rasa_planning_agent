4eb94c6dbc3ddd1acef908c9002177e061785ead
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-04-28 11:16:02 +0200

    Fix if error and change display

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -266,7 +266,7 @@ $("#monday_morning_1").click(function () {
 	console.log("user has clicked monday morning")
 	if(time_slot.classList.contains("toggleable")){
 		console.log("user has clicked monday morning and it was toggleable")
-		if (time_slot.innerHTML = "Walk 30 minutes") {
+		if (time_slot.innerHTML == "Walk 30 minutes") {
 			time_slot.innerHTML = "";
 		  } else {
 			time_slot.innerHTML = "Walk 30 minutes";
