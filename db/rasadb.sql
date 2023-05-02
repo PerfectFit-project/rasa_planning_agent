04827b9d6a19a8b73f6c1b879c12fcf62980f7dd
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-05-02 10:07:06 +0200

    Change db tables

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -1,7 +1,9 @@
 use db;
 
-CREATE TABLE users(prolific_id TEXT, name TEXT, time DATETIME);
+CREATE TABLE users(prolific_id TEXT, time DATETIME, event TEXT);
 
 
-CREATE TABLE sessiondata(prolific_id TEXT, session_num TEXT, response_type TEXT,
-response_value TEXT, time DATETIME)
+CREATE TABLE sessiondata(prolific_id TEXT, time DATETIME, goal TEXT, plan_1 TEXT, plan_2 TEXT, plan_3 TEXT, reward TEXT);
+
+
+CREATE TABLE state_action_state(prolific_id TEXT, time DATETIME, state_before TEXT, action TEXT, state_after TEXT);
