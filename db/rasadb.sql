3c1552ee72cbad2080b55539529d9e78b5b441d4
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-05-08 11:41:34 +0200

    Fix database script

--- db/rasadb.sql
+++ db/rasadb.sql
@@ -1,9 +1,9 @@
 use db;
 
-CREATE TABLE users(prolific_id TEXT, time DATETIME, event TEXT);
+CREATE TABLE sessiondata(prolific_id TEXT, time DATETIME, event TEXT);
 
 
-CREATE TABLE sessiondata(prolific_id TEXT, time DATETIME, goal TEXT, plan_1 TEXT, plan_2 TEXT, plan_3 TEXT, reward TEXT);
+CREATE TABLE users(prolific_id TEXT, time DATETIME, goal TEXT, plan_1 TEXT, plan_2 TEXT, plan_3 TEXT, reward TEXT);
 
 
 CREATE TABLE state_action_state(prolific_id TEXT, time DATETIME, state_before TEXT, action TEXT, state_after TEXT);
