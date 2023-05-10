710bb609396ea30a879f1e4a645e10b16c8cc865
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-05-10 23:02:05 +0200

    Rewording

--- actions/actions.py
+++ actions/actions.py
@@ -691,7 +691,7 @@ class ActionSelectAction(Action):
 
             if num_actions == 2:
 
-                dispatcher.utter_message(text="I am about to ask the three questions regarding your situation again.")
+                dispatcher.utter_message(text="I am going to ask the three questions regarding your situation again.")
 
             elif num_actions == 4:
 
