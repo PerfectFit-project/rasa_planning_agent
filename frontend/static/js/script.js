4cb7dd43d2dc5443e844835aa8592acfc1f17d4b
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-05-10 22:48:02 +0200

    Add back delay between messages

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -472,8 +472,8 @@ function setBotResponse(response) {
 	//if there is more than 1 message from the bot
 	if (response.length > 1){
 		//show typing symbol again
-		// var delay_typing = 600 + delay_first_message;
-		var delay_typing = 20;
+		var delay_typing = 600 + delay_first_message;
+		// var delay_typing = 20;
 		setTimeout(function () {
 		showBotTyping();
 		}, delay_typing)
@@ -483,7 +483,7 @@ function setBotResponse(response) {
 		for (var i = 1; i < response.length; i++){
 			
 			//Add delay based on the length of the next message
-			// summed_timeout += Math.min(Math.max(response[i].text.length * 45, 800), 5000);
+			summed_timeout += Math.min(Math.max(response[i].text.length * 45, 800), 5000);
 			doScaledTimeout(i, response, summed_timeout)
 			
 		}
