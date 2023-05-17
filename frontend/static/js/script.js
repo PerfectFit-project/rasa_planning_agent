dcd8a3a583a88209ea5f1ed588c6ef965964e303
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-05-17 11:05:46 +0200

    Try to fix displaying plan

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -356,12 +356,14 @@ function setBotResponse(response) {
 
 						document.getElementById("month_3").innerHTML = "Walking for up to " + month_3 + " hours per week, distributed across 6 time slots each week";
 
-						$(".plan").toggle();
+						$(".plan_table").toggle();
+
+						$("#plan_first_2_weeks").toggle();
 
 						number_plan = 1;
 					}
 					else if(response_text[j].includes("That's right, your first walk is on")){
-						$(".plan_after_week_2").toggle();
+						$("#plan_after_week_2").toggle();
 					}
 					else if(response_text[j].includes("this is a message for javascript: enable the buttons")){
 
