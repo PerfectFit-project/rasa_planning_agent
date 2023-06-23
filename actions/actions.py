6a493b2b84998427f9975f9b097ffa2b37b9c56f
Andrei <56655325+AndrewStefan@users.noreply.github.com>
2023-06-23 14:51:27 +0200

    Move utter to the right place

--- actions/actions.py
+++ actions/actions.py
@@ -929,10 +929,11 @@ class ActionSaveGoalPlansAndReward(Action):
 
             planning_importance_explanation = tracker.get_slot("planning_importance_explanation")
 
-            dispatcher.utter_message(text=f"{prolific_id}, {formatted_date}, {goal}, {plan_1}, {plan_2}, {plan_3}, {reward}, {testimonial_1}, {takeaway_1}, {testimonial_2}, {takeaway_2}, {identified_barrier}, {barrier_descripion}, {barrier_strategy_1}, {barrier_strategy_2}, {planning_relevance}, {planning_importance_explanation}")
 
 
             reward = f"Reward: satifaction = {satisfaction}, commitment_1 = {commitment_1}, commitment_f = {commitment_f}, confidence_goal = {confidence_goal}"
+
+            dispatcher.utter_message(text=f"{prolific_id}, {formatted_date}, {goal}, {plan_1}, {plan_2}, {plan_3}, {reward}, {testimonial_1}, {takeaway_1}, {testimonial_2}, {takeaway_2}, {identified_barrier}, {barrier_descripion}, {barrier_strategy_1}, {barrier_strategy_2}, {planning_relevance}, {planning_importance_explanation}")
 
             save_goal_plans_and_reward_to_db(cur, conn, prolific_id, formatted_date, goal, plan_1, plan_2, plan_3, reward, testimonial_1, takeaway_1, testimonial_2, takeaway_2, identified_barrier, barrier_descripion, barrier_strategy_1, barrier_strategy_2, planning_relevance, planning_importance_explanation)
 
